50a5860929e45f917440c362640ba4e9d1338d4b
Emily Alice Michael <emilyam@protonmail.com>
2020-09-09 16:08:02 -0800

    Parse arguments for problem to solve

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = std::env::args();
+    let a1 = a.next();
+    let a2 = a.next();
+
+    if let Some(cmd) = a1 {
+        match a2 {
+            Some(n) => {
+                solve(n.parse::<u32>().unwrap());
+            }
+            None => { println!("Provide a problem number as so: \"{} n\"", cmd); }
+        }
+    }
+}
+
+fn solve(n: u32) {
+    match n {
+        0 => { println!("Project Euler is not zero-indexed.") }
+        _ => { println!("Problem {} has not yet been solved.", n); }
+    }
 }
