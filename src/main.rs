406444c6044e2abffbc84530fd2ef1e6537a1235
Emily Alice Michael <emilyam@protonmail.com>
2020-09-11 07:49:26 -0800

    Allow selecting optimized solutions from command line

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 mod helpers;
+mod optimized_problems;
 mod problems;
 
-fn main() {
-    let mut a = std::env::args();
-    let a1 = a.next();
-    let a2 = a.next();
+use clap::{value_t, App, Arg};
 
-    if let Some(cmd) = a1 {
-        match a2 {
-            Some(n) => {
-                solve(n.parse::<usize>().unwrap());
-            }
-            None => {
-                println!("Provide a problem number as so: \"{} n\"", cmd);
-            }
-        }
-    }
+fn main() {
+    let matches = App::new("euler")
+        .about("Solutions for Project Euler problems up to 100")
+        .author("Emily A. Michael <emilyam@protonmail.com>")
+        .arg(
+            Arg::with_name("optimized")
+                .short("o")
+                .long("optimized")
+                .help(
+                    "Whether to use the optimized solution, if both an optimized and an \
+                    unoptimized solution exist",
+                )
+                .takes_value(true)
+                .possible_values(&["true", "false"])
+                .default_value("true"),
+        )
+        .arg(Arg::with_name("problem").index(1).required(true))
+        .get_matches();
+
+    let problem = value_t!(matches.value_of("problem"), usize).unwrap_or_else(|e| e.exit());
+    let optimized = value_t!(matches.value_of("optimized"), bool).unwrap_or_else(|e| e.exit());
+
+    solve(problem, optimized);
 }
 
-fn solve(n: usize) {
+fn solve(n: usize, optimized: bool) {
+    use optimized_problems::*;
     use problems::*;
     use std::collections::HashMap;
 
@@ -40,7 +52,14 @@ fn solve(n: usize) {
         (15, &p15),
     ];
     let problems: HashMap<usize, &dyn Fn() -> String> = problems.iter().cloned().collect();
-    if problems.contains_key(&n) {
+
+    let optimized_problems: Vec<(usize, &dyn Fn() -> String)> = vec![];
+    let optimized_problems: HashMap<usize, &dyn Fn() -> String> =
+        optimized_problems.iter().cloned().collect();
+
+    if optimized && optimized_problems.contains_key(&n) {
+        println!("{}", (*(optimized_problems.get(&n).unwrap())()).to_string());
+    } else if problems.contains_key(&n) {
         println!("{}", (*(problems.get(&n).unwrap())()).to_string());
     } else {
         println!("No such solution.");
