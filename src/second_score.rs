533408607735995a8f10fcc4d13f29d50c07a30b
Emily Alice Michael <emilyam@protonmail.com>
2020-09-13 14:10:26 -0800

    Solve problem 22

--- src/second_score.rs
+++ src/second_score.rs
@@ -29,3 +29,30 @@ pub fn p21() -> String {
         .sum::<usize>();
     amicable_sum.to_string()
 }
+
+/// Finds the total of the "name scores" in a file provided,
+/// where the "name score" is the product of the (1-indexed) position
+/// of the name in alphabetical order and the sum of the position
+/// in the alphabet of each letter in the name.
+pub fn p22() -> String {
+    // Get list of names and sort
+    let raw_file = String::from_utf8(include_bytes!("p022_names.txt").to_vec()).unwrap();
+    let mut names: Vec<&str> = raw_file.split(',').collect();
+    names.sort();
+
+    // Calculate the scores for each name and sum them
+    let mut sum: u32 = 0;
+    for (index, &name) in names.iter().enumerate() {
+        let bytes = name.as_bytes();
+        let mut score = 0;
+        for b in bytes {
+            let base = 'A' as u8 - 1;
+            if b > &base {
+                score += b - &base;
+            }
+        }
+        sum += score as u32 * (index as u32 + 1);
+    }
+
+    sum.to_string()
+}
